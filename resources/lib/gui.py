44edb240a143e327ab24242320588e3ceb0431dd
Guido De Rosa <job@guidoderosa.net>
2011-10-10 17:31:40 +0200

    some ugly work on gui, borrwing from xbmc audio mixer

--- resources/lib/gui.py
+++ resources/lib/gui.py
@@ -1,7 +1,13 @@
+import xbmc
 import xbmcgui
 
-class GUI( xbmcgui.WindowXMLDialog ):
+class GUI( xbmcgui.WindowXML ):
     def __init__( self, *args, **kwargs ):
         pass
+
+    def onInit(self):
+        print "vera: GUI.onInit"
+        label = xbmcgui.ControlLabel(100,100,100,100,'VAFFA');
+        self.addControl(label)
 
 
