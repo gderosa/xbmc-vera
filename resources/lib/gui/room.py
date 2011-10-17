41edee9ee137bae1d563ac6f799f5057197bf6bf
Guido De Rosa <job@guidoderosa.net>
2011-10-17 12:32:14 +0200

    split GUI and remove a useless button from 'room'

---
+++ resources/lib/gui/room.py
@@ -0,0 +1,52 @@
+import xbmc
+import xbmcgui
+import xbmcaddon
+
+import vera
+
+import controlid.room
+
+class RoomUI( xbmcgui.WindowXMLDialog ):
+    def __init__(self, *args, **kwargs):
+        self.room = kwargs['room']
+        self.vera = kwargs['vera']
+
+    def onInit(self):
+        self.hideDevices()
+        label = self.getControl(10101)
+        if self.room:
+            label.setLabel(self.room['name'])
+        else:
+            label.setLabel('Devices not in any room')
+        self.updateDevices()
+
+    def onClick(self, controlID):
+        pass
+
+    def updateDevices(self):
+        devices = self.vera.data['devices']
+
+        controlID = controlid.room.DEVICE_FIRST
+        for device in devices:
+            if self.room:
+                if device['room'] == self.room['id'] :
+                    self.showLabel(controlID, device['name'])
+                    controlID += 1
+            else:
+                if device['room'] == 0:
+                    self.showLabel(controlID, device['name'])
+                    controlID += 1
+
+    def showLabel(self, controlID, label): # TODO: DRY
+        control = self.getControl(controlID)
+        control.setVisible(True)
+        control.setLabel(label)
+
+    def hideDevices(self, first=controlid.room.DEVICE_FIRST):
+        for controlID in range(first, controlid.room.DEVICE_LAST + 1):
+            button = self.getControl(controlID)
+            button.setVisible(False)
+
+
+
+
