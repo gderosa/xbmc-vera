f3cecfa5606b071a5d07509aaca435a05a2a88d8
Guido De Rosa <job@guidoderosa.net>
2011-10-26 23:56:03 +0200

    toggle motion sensor

--- resources/lib/vera/device/__init__.py
+++ resources/lib/vera/device/__init__.py
@@ -2,15 +2,27 @@ from vera.device import category
 
 # turn on/off a BinaryLight, arm/disarm a motion sensor etc.
 def toggle(device, vera_controller):
+    
     if device['category'] == category.BINARY_LIGHT:
         newTargetValue = '1'
         if int(device['status']):
             newTargetValue = '0'
         vera_controller.GET ( \
-'/data_request?id=action&DeviceNum=' + str(device['id'])          + \
-'&serviceId=urn:upnp-org:serviceId:SwitchPower1&action=SetTarget' + \
-'&newTargetValue=' + newTargetValue                                 \
+'/data_request?id=action&DeviceNum=' + str(device['id'])                    + \
+'&serviceId=urn:upnp-org:serviceId:SwitchPower1&action=SetTarget'           + \
+'&newTargetValue=' + newTargetValue                                           \
         )
+
+    elif device['category'] == category.MOTION_SENSOR:
+        Value = '1'
+        if int(device['armed']):
+            Value = '0'
+        vera_controller.GET ( \
+'/data_request?id=variableset&DeviceNum=' + str(device['id'])               + \
+'&serviceId=urn:micasaverde-com:serviceId:SecuritySensor1&Variable=Armed'   + \
+'&Value=' + Value
+        )
+
 
 
 
