4ecd3848932024072fc681edda2224ccf620920d
Guido De Rosa <job@guidoderosa.net>
2011-11-09 18:01:20 +0100

    dimmer: key mapping and transparent background

--- resources/lib/gui/popup.py
+++ resources/lib/gui/popup.py
@@ -9,8 +9,11 @@ import vera.device
 import gui.controlid as controlid
 import controlid.dimmable_light
 
-# keymap.xml ?
-ACTION_PREVIOUS_MENU = 10
+# See xbmc/guilib/Key.h in XMBC sources
+ACTION_MOVE_UP          = 3
+ACTION_MOVE_DOWN        = 4
+ACTION_ENTER            = 7
+ACTION_PREVIOUS_MENU    = 10
 
 __addon__   = xbmcaddon.Addon('script.vera')
 __cwd__     = __addon__.getAddonInfo('path')
@@ -24,12 +27,13 @@ class DimLightThread(threading.Thread):
         self.vera = gui_.parent.vera
 
     def run(self):
+        self.runThread = True
         slider = self.gui.getControl(controlid.dimmable_light.SLIDER)
-        while(True):
-            time.sleep(1)
+        while(self.runThread):
             newValue = slider.getPercent()
             if newValue != int( float( self.device['level'] ) ):  
                 vera.device.dim(self.device, self.vera, newValue)
+            time.sleep(1)
 
 class DimmableLight( xbmcgui.WindowXMLDialog ):
 
@@ -39,9 +43,20 @@ class DimmableLight( xbmcgui.WindowXMLDialog ):
         self.dimmerThread = DimLightThread(self)
 
     def onInit(self):
-        slider = self.getControl(controlid.dimmable_light.SLIDER)
-        slider.setPercent(int(float(self.device['level'])))  
-
+        self.slider().setPercent( int( float( self.device['level'] ) ) )
         self.dimmerThread.start()
+
+    def slider(self):
+        return self.getControl(controlid.dimmable_light.SLIDER)
+
+    def onAction(self, action):
+        if action in (ACTION_PREVIOUS_MENU, ACTION_ENTER):
+            self.dimmerThread.runThread = False
+            self.close()
+        elif action == ACTION_MOVE_UP:
+            self.slider().setPercent(100)
+        elif action == ACTION_MOVE_DOWN:
+            self.slider().setPercent(0)
+
 
 
