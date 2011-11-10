1c0f29dc51a5cb2021f54e9770617fdd1476c3c5
Guido De Rosa <job@guidoderosa.net>
2011-11-10 05:04:38 +0100

    hvac popup: arrows id

--- resources/lib/gui/controlid/hvac.py
+++ resources/lib/gui/controlid/hvac.py
@@ -7,3 +7,13 @@ FAN         = 105005
 
 LABEL_HEAT  = 105103
 LABEL_COOL  = 105104
+
+ARROWS_MODE = 105202
+ARROWS_HEAT = 105203
+ARROWS_COOL = 105204
+ARROWS_FAN  = 105205
+
+def controlToArrows(controlID):
+    return controlID + 200
+
+
