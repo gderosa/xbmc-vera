3cba92efcee02ff3fab9d33356512642329b37c2
Guido De Rosa <job@guidoderosa.net>
2012-01-04 16:18:20 +0100

    camera: fix action/exit

--- resources/lib/gui/popup/camera.py
+++ resources/lib/gui/popup/camera.py
@@ -3,7 +3,9 @@ import  socket
 import  xbmcaddon
 import  xbmcgui
 
-import gui.popup
+from    gui.xbmc    import *
+
+import  gui.popup
 
 __addon__   = xbmcaddon.Addon('script.vera')
 __cwd__     = __addon__.getAddonInfo('path')
@@ -16,23 +18,13 @@ class Camera( xbmcgui.WindowXMLDialog ):
         self.vera   = self.parent.vera
 
     def onInit(self):
-        self.update()
+        pass
 
     def onAction(self, action):
-        try:
-            focusedControl = self.getFocus()
-        except TypeError:
-            return
-
         if      action == ACTION_PREVIOUS_MENU:
             self.close()
         elif    action == ACTION_ENTER:
             self.close()
-        else:
-            pass
-
-    def update(self):
-        pass
 
 
 # Compat
