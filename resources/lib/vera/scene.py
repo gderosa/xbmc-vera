48002ed244859b8344f650dad1e5863a32e06478
Guido De Rosa <job@guidoderosa.net>
2012-01-05 19:49:52 +0100

    Check that a scene is active before running it

--- resources/lib/vera/scene.py
+++ resources/lib/vera/scene.py
@@ -1,8 +1,8 @@
 # http://wiki.micasaverde.com/index.php/UI_Simple#Status_of_the_device_or_scene_and_control_buttons
 
-# TODO: check that the scene status is 'active'
 def run(scene, vera_controller):
-    vera_controller.GET( \
+    if scene['active']:
+        vera_controller.GET( \
 '/data_request?id=lu_action'                                        + \
 '&serviceId=urn:micasaverde-com:serviceId:HomeAutomationGateway1'   + \
 '&action=RunScene&SceneNum=' + str(scene['id'])                     ) 
